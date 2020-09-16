a928ac0a1b517340fef624d2ee08af4e4ecc7248
AvakovArt <UC.AA.tshk.18@gmail.com>
2020-09-16 15:46:30 +0500

    added slider

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+  var slider = new Swiper(".season__items", {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 3,
+    autoHover: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".season__items-button--next",
+      prevEl: ".season__items-button--prev",
+    },
+  });
+});
