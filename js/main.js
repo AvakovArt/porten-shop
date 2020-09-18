1d2357439b8749785c44f7aaff6fffe92e2187e7
AvakovArt <UC.AA.tshk.18@gmail.com>
2020-09-18 09:36:36 +0500

    create adaptive

--- js/main.js
+++ js/main.js
@@ -10,5 +10,18 @@ $(document).ready(function () {
       nextEl: ".season__items-button--next",
       prevEl: ".season__items-button--prev",
     },
+    breakpoints: {
+      320: { slidesPerView: 1 },
+      500: { slidesPerView: 2 },
+      576: { slidesPerView: 2 },
+      768: { slidesPerView: 2 },
+      992: { slidesPerView: 2 },
+      // 1140: {},
+      1500: {},
+    },
+  });
+  $(".navbar__burger").click(function (event) {
+    $(".navbar__burger,.navbar__links").toggleClass("open");
+    $("body").toggleClass("lock");
   });
 });
